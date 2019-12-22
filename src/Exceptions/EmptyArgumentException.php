9d2e36e11b007586c31b5cbda6c010d7e44343d5
Serge Kudashev <kudashevs@gmail.com>
2019-12-22 17:54:58 +0300

    Add Exceptions add main and specific exceptions for arguments handler

---
+++ src/Exceptions/EmptyArgumentException.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * This file is part of Benchmark PHP.
+ *
+ * (c) Sergey Kudashev <kudashevs@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace BenchmarkPHP\Exceptions;
+
+class EmptyArgumentException extends ArgumentsHandlerException
+{
+}
